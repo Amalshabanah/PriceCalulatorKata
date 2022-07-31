0a14959808ce8f949e57338df2a688db691460e5
Amalshabanah <amalshabanah7@gmail.com>
2022-07-31 15:37:52 +0300

    "Tax"

---
+++ PriceCalKata/Book.cs
@@ -0,0 +1,8 @@
+namespace Price_Calculator_Kata;
+
+public class Book : Product
+{
+    public Book(string name, int upc, double price) : base(name, upc, price)
+    {
+    }
+}
---
+++ PriceCalKata/Product.cs
@@ -0,0 +1,19 @@
+namespace Price_Calculator_Kata;
+
+public class Product
+{
+    public string ProductName { get; set; }
+    public int Upc { get; set; }
+    public double Price { get; set; }
+
+    public double Tax { get; set; }
+    public Product(string name, int upc, double price)
+    {
+        ProductName = name;
+        Upc = upc;
+        Price = price;
+    }
+    public double PriceAfterTax(double price, double tax) => price + price * (tax/100);
+  
+
+}
