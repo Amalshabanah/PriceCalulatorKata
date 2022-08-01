53e97297fc448375543334b5dd4a022bc965978f
Amalshabanah <amalshabanah7@gmail.com>
2022-08-01 05:59:33 +0300

    "Discount"

--- PriceCalKata/Product.cs
+++ PriceCalKata/Product.cs
@@ -6,14 +6,17 @@ public class Product
     public int Upc { get; set; }
     public double Price { get; set; }
 
-    public double Tax { get; set; }
+    //public double Tax { get; set; }
+    
     public Product(string name, int upc, double price)
     {
         ProductName = name;
         Upc = upc;
         Price = price;
     }
-    public double PriceAfterTax(double price, double tax) => price + price * (tax/100);
+    public double TaxAmount(double price, double tax) =>  price * (tax/100);
+    public double DiscountAmount(double price, double discount) => price * (discount/100);
+
   
 
 }
--- PriceCalKata/Program.cs
+++ PriceCalKata/Program.cs
@@ -9,13 +9,20 @@ namespace Price_Calculator_Kata
         public static void Main(string[] args)
         {
             Book product = new Book("The Little Prince", 12345, 20.25);
+            
             Console.WriteLine("Please Specify The Tax");
-            product.Tax = Convert.ToDouble(Console.ReadLine());
-            double PriceAfter = product.PriceAfterTax(product.Price, product.Tax);
+            double tax = Convert.ToDouble(Console.ReadLine());
+            
+            Console.WriteLine("Please Specify The Discount");
+            double discount = Convert.ToDouble(Console.ReadLine());
+            
+            double PriceAfter = product.Price + product.TaxAmount(product.Price, tax) - product.DiscountAmount(product.Price , discount);
             var pprice = PriceAfter.ToString("0.00");
             Console.WriteLine($"Sample product: Book with name = {product.ProductName}, UPC= {product.Upc}, price= ${product.Price}.");
-            Console.WriteLine($"Product price reported as ${product.Price}  before tax, and {pprice} after {product.Tax}% tax.");
-
+            //Console.WriteLine($"Product price reported as ${product.Price}  before tax, and {pprice} after {product.Tax}% tax.");
+            Console.WriteLine(
+                $"Tax = {tax}% ,discount = {discount}% , Tax amount = {product.TaxAmount(product.Price, tax)} ," +
+                $" Discount amount = {product.DiscountAmount(product.Price , discount)} , Price before = ${product.Price}, price after = ${pprice}");
 
         }
     }
