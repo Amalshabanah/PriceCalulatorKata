86b46daecdbf108374e98276fd8eba4d400e40d9
Amalshabanah <amalshabanah7@gmail.com>
2022-08-07 12:12:11 +0300

    Revert ""Discount""
    This reverts commit 53e97297fc448375543334b5dd4a022bc965978f.

---
+++ PriceCalKata/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+
+namespace Price_Calculator_Kata
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            Book product = new Book("The Little Prince", 12345, 20.25);
+            Console.WriteLine("Please Specify The Tax");
+            product.Tax = Convert.ToDouble(Console.ReadLine());
+            double PriceAfter = product.PriceAfterTax(product.Price, product.Tax);
+            var pprice = PriceAfter.ToString("0.00");
+            Console.WriteLine($"Sample product: Book with name = {product.ProductName}, UPC= {product.Upc}, price= ${product.Price}.");
+            Console.WriteLine($"Product price reported as ${product.Price}  before tax, and {pprice} after {product.Tax}% tax.");
+
+
+        }
+    }
+
+}
