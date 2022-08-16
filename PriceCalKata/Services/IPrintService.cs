83fb2c61d113f7b8752b214c2f6f711e7a44dd71
Amalshabanah <amalshabanah7@gmail.com>
2022-08-16 11:22:56 +0300

    Added IprintData & print data files

--- PriceCalKata/Services/IPrintService.cs
+++
@@ -1,8 +0,0 @@
-namespace PriceCalKata.Services;
-
-public interface IPrintService
-{
-    public  void  PrintInfo(string productName, double upc, double price);
-
-    public  void PrintTax(double price, double tax, double priceAfterTax);
-}
