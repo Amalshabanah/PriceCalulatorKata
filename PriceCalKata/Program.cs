e1678e1252e607b00898c5864f2d47d66cac940c
Amalshabanah <amalshabanah7@gmail.com>
2022-08-18 17:49:23 +0300

    Report - Rename priceAfterTax to finalPrice before Megre.

--- PriceCalKata/Program.cs
+++ PriceCalKata/Program.cs
@@ -10,7 +10,7 @@ namespace PriceCalKata
             var productRepo = new ProductRepository();
             var productPrint = new ProductPrintService();
             var productService = new ProductService(productRepo , productPrint);
-          
+            
             productService.CalculateAndPrintPriceInfo();
         }
     }
--- PriceCalKata/Services/ProductPrintService.cs
+++ PriceCalKata/Services/ProductPrintService.cs
@@ -3,13 +3,23 @@ using PriceCalKata.Models;
 namespace PriceCalKata.Services;
 public class ProductPrintService :  IProductPrintService
 {
-    public void PrintPriceInfo(Product product , double priceAfterTax)
+    public void PrintTaxInfo(Product product , double priceAfterTax)
+    {
+        Console.WriteLine($"Sample product : Book with name = {product.ProductName} , UPC = {product.Upc} , "+
+                          $"price = ${product.Price.ToString("0.00")}.");
+        
+        Console.WriteLine($"Product price reported as ${product.Price.ToString("0.00")} before tax ," +
+                          $" and ${priceAfterTax} " +
+                          $"after {product.Tax}% tax.\n");
+    }
+    
+    public void PrintPriceInfo(Product product , double finalPrice)
     {
         Console.WriteLine($"Sample product : Book with name = {product.ProductName} , UPC = {product.Upc} , "+
                           $"price = ${product.Price.ToString("0.00")}.");
 
         Console.WriteLine($"Price Before : ${product.Price.ToString("0.00")} ," +
-                          $" Price After : ${priceAfterTax}");
+                          $" Price After : ${finalPrice}");
     }
 
     public void PrintDeductedAmount(double discountAmount)
