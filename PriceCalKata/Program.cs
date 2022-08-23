a6ab2841ded73ede3b8e52548986ef81ae5b2f71
Amalshabanah <amalshabanah7@gmail.com>
2022-08-23 18:36:33 +0300

    Currency - Remove Extra line.

--- PriceCalKata/Program.cs
+++ PriceCalKata/Program.cs
@@ -12,7 +12,6 @@ namespace PriceCalKata
             var productService = new ProductService(productRepo, productPrint);
             
             productService.CalculateAndPrintPriceWithCurrencyInfoAfterTax();
-            
         }
     }
 }
